abf1763d715b4f539820932699ea0bd2f8b5fa9e
Vamshireddy199 <166207205+Vamshireddy199@users.noreply.github.com>
2024-12-23 12:28:31 +0530

    admin.js

---
+++ pages/admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Admin Page</h1>
+      {/* Your admin page content goes here */}
+    </div>
+  );
+};
+
+export default AdminPage;
